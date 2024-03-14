68d955f4d953ddc7653a9b5f3ffc205fccd55167
Bailey <hen.baileyk@gmail.com>
2024-03-14 13:27:42 -0700

    Key press for search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -16,11 +16,24 @@ const SearchBar = (props) => {
         props.onSearch(term);
     }, [props.onSearch, term]);
 
+    const handleKeyUp = useCallback((event) => {
+        console.log('Key pressed:', event.key); 
+        if (event.key === 'Enter') {
+            console.log('Enter key pressed, initiating search');
+            search();
+        }
+    }, [search]);
+
     return(
         <div className="main-search-container">
             <div className="search-container">
                 <FontAwesomeIcon icon={faMagnifyingGlass} style={{color: "#ffffff",}} />
-                <input type="search" className="search-input" placeholder="What song are you looking for?" onChange={handleTermChange}/>
+                <input 
+                    type="search" 
+                    className="search-input" 
+                    placeholder="What song are you looking for?" 
+                    onChange={handleTermChange}
+                    onKeyUp={handleKeyUp} />
                 <button className="search-btn" onClick={search}>Search</button>
             </div>
         </div>
