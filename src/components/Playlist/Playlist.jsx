efc89ae87f05d1da764795808f85516870604713
Bailey <hen.baileyk@gmail.com>
2024-03-14 15:36:43 -0700

    playlist input clear

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,22 +1,32 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState } from "react";
 
 import TrackList from '../TrackList/TrackList';
 import './Playlist.css'
 
 const Playlist = (props) => {
 
+    const [playlistName, setPlaylistName] = useState('');
+
     const handleNameChange = useCallback(
         (event) => {
+          setPlaylistName(event.target.value);
           props.onNameChange(event.target.value);
         },
         [props.onNameChange]
       );
 
+    const handleSave = useCallback(() => {
+        props.onSave();
+        setPlaylistName(''); 
+    }, [props.onSave]);
+
     return(
         <div className="playlist-container">
             <div className="playlist-name-container">
-                <input onChange={handleNameChange} placeholder="Enter Playlist Name"/>
-                <button className="playlist-btn" onClick={props.onSave}>Save</button>
+                <input onChange={handleNameChange} 
+                       placeholder="Enter Playlist Name"
+                       value={playlistName}/>
+                <button className="playlist-btn" onClick={handleSave}>Save</button>
             </div>
             <div>
                 <TrackList
